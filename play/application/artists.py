54b864f9083a27654f1be8d4fd3894db065a0df6
Julian Hille <j.hille484@gmail.com>
2015-09-23 23:05:16 +0200

    Adds artists route

---
+++ play/application/artists.py
@@ -0,0 +1,29 @@
+from play.application.blueprint import Blueprint
+
+SCHEMA = {
+    'item_title': 'artist',
+    'public_item_methods': [],
+    'item_methods': ['GET'],
+    'resource_methods': ['GET'],
+    'allowed_roles': ['user'],
+    'schema': {
+        'gerne': {
+            'type': 'list',
+            'schema': {
+                'type': 'string'
+            }
+        },
+        'media': {
+            'type': 'list',
+            'schema': {
+                'type': 'string'
+            }
+        },
+        'name': {
+            'type': 'string'
+        }
+    }
+}
+
+
+blueprint = Blueprint('artists', __name__, SCHEMA)
--- play/application/wsgi.py
+++ play/application/wsgi.py
@@ -1,5 +1,5 @@
 from play.application.application import Application
-from play.application import albums, playlists, tracks
+from play.application import albums, artists, playlists, tracks
 
 settings = {
     'DOMAIN': {
@@ -10,6 +10,7 @@ settings = {
 def application():
     app = Application(settings=settings)
     app.register_blueprint(albums.blueprint)
+    app.register_blueprint(artists.blueprint)
     app.register_blueprint(playlists.blueprint)
     app.register_blueprint(tracks.blueprint)
     return app.instantiate()
