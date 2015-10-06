3115a911b3d126b0c4288327f77cb7a695bf0572
Julian Hille <julian.hille@project-collins.com>
2015-10-07 01:41:27 +0200

    Adds index creation utility

---
+++ play/mongo.py
@@ -0,0 +1,15 @@
+import pymongo
+
+
+def ensure_indices(db): # nocov
+    # users
+    db.users.ensure_index([('name', pymongo.DESCENDING)], unique=True, background=True)
+
+    # tracks
+    db.tracks.ensure_index([('name', pymongo.TEXT)], background=True)
+    db.tracks.ensure_index([('hash', pymongo.DESCENDING)], background=True)
+
+    # playlists
+    db.playlists.ensure_index([('name', pymongo.TEXT)], background=True)
+    db.playlists.ensure_index([('owner', pymongo.ASCENDING)], background=True)
+    db.playlists.ensure_index([('public', pymongo.ASCENDING)], background=True)
