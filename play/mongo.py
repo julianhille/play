ad491234004a6920ea966b2c367c7ea09126a7fa
Julian Hille <julian.hille@project-collins.com>
2015-10-17 13:30:05 +0200

    Changes deprecated ensure_index to create_index

--- play/mongo.py
+++ play/mongo.py
@@ -8,17 +8,17 @@ def ensure_indices(db):  # nocov
     db.artists.create_index([('discogs_id', pymongo.DESCENDING)], background=True)
 
     # users
-    db.users.ensure_index([('name', pymongo.DESCENDING)], unique=True, background=True)
+    db.users.create_index([('name', pymongo.DESCENDING)], unique=True, background=True)
 
     # tracks
-    db.tracks.ensure_index([('search', pymongo.TEXT)], background=True)
-    db.tracks.ensure_index([('hash', pymongo.DESCENDING)], background=True)
+    db.tracks.create_index([('search', pymongo.TEXT)], background=True)
+    db.tracks.create_index([('hash', pymongo.DESCENDING)], background=True)
 
     # directories
-    db.directories.ensure_index([('path', pymongo.ASCENDING)], background=True, unique=True)
-    db.directories.ensure_index([('name', pymongo.ASCENDING)], background=True)
+    db.directories.create_index([('path', pymongo.ASCENDING)], background=True, unique=True)
+    db.directories.create_index([('name', pymongo.ASCENDING)], background=True)
 
     # playlists
-    db.playlists.ensure_index([('name', pymongo.TEXT)], background=True)
-    db.playlists.ensure_index([('owner', pymongo.ASCENDING)], background=True)
-    db.playlists.ensure_index([('public', pymongo.ASCENDING)], background=True)
+    db.playlists.create_index([('name', pymongo.TEXT)], background=True)
+    db.playlists.create_index([('owner', pymongo.ASCENDING)], background=True)
+    db.playlists.create_index([('public', pymongo.ASCENDING)], background=True)
