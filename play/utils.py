77c93672ef9f8af332c8887e23cd28e3e4e45d7c
Julian Hille <j.hille484@gmail.com>
2015-12-28 20:46:55 +0100

    Config with default values

--- play/utils.py
+++ play/utils.py
@@ -1,5 +1,12 @@
+import logging
 
 
 def delete_track_post_process(db, track_id):
     db.playlists.update(
         {'tracks': {'$in': [track_id]}}, {'$pull': {'tracks': track_id}}, multi=True)
+
+
+def add_logging(application):
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.INFO)
+    application.logger.addHandler(handler)
