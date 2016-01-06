bbc8fac5d43eba726e0a692ab7ddae1969c626dc
Julian Hille <j.hille484@gmail.com>
2016-01-06 01:19:09 +0100

    Fixes a validator issue, which requires at least a dict as schema

--- tests/api/test_application.py
+++ tests/api/test_application.py
@@ -4,7 +4,7 @@ from unittest.mock import Mock, patch
 
 
 def test_roles_validator_roles_not_list():
-    validator = application.Validator()
+    validator = application.Validator({})
     validator._error = Mock()
     validator._validate_roles(None, 'field', None)
     assert validator._error.call_count == 1
@@ -12,7 +12,7 @@ def test_roles_validator_roles_not_list():
 
 
 def test_roles_validator_roles_list_not_str():
-    validator = application.Validator()
+    validator = application.Validator({})
     validator._error = Mock()
     validator._validate_roles([None], 'field', [1])
     assert validator._error.call_count == 1
@@ -21,7 +21,7 @@ def test_roles_validator_roles_list_not_str():
 
 def test_path_validator_invalid_path(file_system):
     with patch('play.api.application.os', FakeOsModule(file_system)):
-        validator = application.Validator()
+        validator = application.Validator({})
         validator._error = Mock()
         validator._validate_path(True, 'field', '/invalid')
     assert validator._error.call_count == 1
@@ -30,7 +30,7 @@ def test_path_validator_invalid_path(file_system):
 
 def test_path_validator_valid_file(file_system):
     with patch('play.api.application.os', FakeOsModule(file_system)):
-        validator = application.Validator()
+        validator = application.Validator({})
         validator._error = Mock()
         validator._validate_path(True, 'field', '/tmp/open/git.test')
     assert validator._error.call_count == 1
@@ -39,7 +39,7 @@ def test_path_validator_valid_file(file_system):
 
 def test_path_validator_valid_path(file_system):
     with patch('play.api.application.os', FakeOsModule(file_system)):
-        validator = application.Validator()
+        validator = application.Validator({})
         validator._error = Mock()
         validator._validate_path(True, 'field', '/tmp/open/')
     assert validator._error.call_count == 0
